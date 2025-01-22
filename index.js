c4424fc31380ae42b77ee5e2356cdbfc9106e464
Chanuka-madushan <chanukamadushan889@gmail.com>
2025-01-22 10:42:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,271 +1,196 @@
-const { spawnSync, spawn } = require('child_process')
+const {
+default: makeWASocket,
+useMultiFileAuthState,
+DisconnectReason,
+jidNormalizedUser,
+getContentType,
+fetchLatestBaileysVersion,
+Browsers
+} = require('@whiskeysockets/baileys')
 
-const { existsSync, writeFileSync } = require('fs')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
+const fs = require('fs')
+const P = require('pino')
+const config = require('./config')
+const qrcode = require('qrcode-terminal')
+const util = require('util')
+const { sms,downloadMediaMessage } = require('./lib/msg')
+const axios = require('axios')
+const { File } = require('megajs')
+const prefix = '.'
 
-const path = require('path')
+const ownerNumber = ['94741469245']
 
-const SESSION_ID = 'levanter_2197e95bd3d9a84cb5940ded697098c87d' // Edit this line only, don't remove ' <- this symbol
+//===================SESSION-AUTH============================
+if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
+if(!config.SESSION_ID) return console.log('')
+const sessdata = config.SESSION_ID
+const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
+filer.download((err, data) => {
+if(err) throw err
+fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
+console.log("Session downloaded ✅")
+})})}
 
-let nodeRestartCount = 0
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
 
-const maxNodeRestarts = 5
+//=============================================
 
-const restartWindow = 30000 // 30 seconds
+async function connectToWA() {
+console.log("Connecting DARK_ADAM-MD 🤡🤘💨...");
+const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
+var { version } = await fetchLatestBaileysVersion()
 
-let lastRestartTime = Date.now()
+const conn = makeWASocket({
+        logger: P({ level: 'silent' }),
+        printQRInTerminal: false,
+        browser: Browsers.macOS("Firefox"),
+        syncFullHistory: true,
+        auth: state,
+        version
+        })
+    
+conn.ev.on('connection.update', (update) => {
+const { connection, lastDisconnect } = update
+if (connection === 'close') {
+if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
+connectToWA()
+}
+} else if (connection === 'open') {
+console.log('😼 Installing... ')
+const path = require('path');
+fs.readdirSync("./plugins/").forEach((plugin) => {
+if (path.extname(plugin).toLowerCase() == ".js") {
+require("./plugins/" + plugin);
+}
+});
+console.log('Plugins installed successful ✅')
+console.log('Bot connected to whatsapp ✅')
 
-function startNode() {
+let up = `DARK_ADAM-MD connected successful ✅\n\nPREFIX: ${prefix}`;
 
-  const child = spawn('node', ['index.js'], { cwd: 'levanter', stdio: 'inherit' })
-
-  child.on('exit', (code) => {
-
-    if (code !== 0) {
-
-      const currentTime = Date.now()
-
-      if (currentTime - lastRestartTime > restartWindow) {
-
-        nodeRestartCount = 0
-
-      }
-
-      lastRestartTime = currentTime
-
-      nodeRestartCount++
-
-      if (nodeRestartCount > maxNodeRestarts) {
-
-        console.error('Node.js process is restarting continuously. Stopping retries...')
-
-        return
-
-      }
-
-      console.log(
-
-        `Node.js process exited with code ${code}. Restarting... (Attempt ${nodeRestartCount})`
-
-      )
-
-      startNode()
-
-    }
-
-  })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://files.catbox.moe/m9rojt.jpg` }, caption: up })
 
 }
+})
+conn.ev.on('creds.update', saveCreds)  
 
-function startPm2() {
-
-  const pm2 = spawn('yarn', ['pm2', 'start', 'index.js', '--name', 'levanter', '--attach'], {
-
-    cwd: 'levanter',
-
-    stdio: ['pipe', 'pipe', 'pipe'],
-
-  })
-
-  let restartCount = 0
-
-  const maxRestarts = 5 // Adjust this value as needed
-
-  pm2.on('exit', (code) => {
-
-    if (code !== 0) {
-
-      // console.log('yarn pm2 failed to start, falling back to node...')
-
-      startNode()
-
-    }
-
-  })
-
-  pm2.on('error', (error) => {
-
-    console.error(`yarn pm2 error: ${error.message}`)
-
-    startNode()
-
-  })
-
-  // Check for infinite restarts
-
-  if (pm2.stderr) {
-
-    pm2.stderr.on('data', (data) => {
-
-      const output = data.toString()
-
-      if (output.includes('restart')) {
-
-        restartCount++
-
-        if (restartCount > maxRestarts) {
-
-          // console.log('yarn pm2 is restarting indefinitely, stopping yarn pm2 and starting node...')
-
-          spawnSync('yarn', ['pm2', 'delete', 'levanter'], { cwd: 'levanter', stdio: 'inherit' })
-
-          startNode()
-
-        }
-
-      }
-
-    })
-
-  }
-
-  if (pm2.stdout) {
-
-    pm2.stdout.on('data', (data) => {
-
-      const output = data.toString()
-
-      console.log(output)
-
-      if (output.includes('Connecting')) {
-
-        // console.log('Application is online.')
-
-        restartCount = 0
-
-      }
-
-    })
-
-  }
-
+conn.ev.on('messages.upsert', async(mek) => {
+mek = mek.messages[0]
+if (!mek.message) return	
+mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+await conn.readMessages([mek.key])
+}
+const m = sms(conn, mek)
+const type = getContentType(mek.message)
+const content = JSON.stringify(mek.message)
+const from = mek.key.remoteJid
+const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
+const isCmd = body.startsWith(prefix)
+const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+const args = body.trim().split(/ +/).slice(1)
+const q = args.join(' ')
+const isGroup = from.endsWith('@g.us')
+const sender = mek.key.fromMe ? (conn.user.id.split(':')[0]+'@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
+const senderNumber = sender.split('@')[0]
+const botNumber = conn.user.id.split(':')[0]
+const pushname = mek.pushName || 'Sin Nombre'
+const isMe = botNumber.includes(senderNumber)
+const isOwner = ownerNumber.includes(senderNumber) || isMe
+const botNumber2 = await jidNormalizedUser(conn.user.id);
+const groupMetadata = isGroup ? await conn.groupMetadata(from).catch(e => {}) : ''
+const groupName = isGroup ? groupMetadata.subject : ''
+const participants = isGroup ? await groupMetadata.participants : ''
+const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
+const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
+const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+const isReact = m.message.reactionMessage ? true : false
+const reply = (teks) => {
+conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
 
-function installDependencies() {
-
-  // console.log('Installing dependencies...')
-
-  const installResult = spawnSync(
-
-    'yarn',
-
-    ['install', '--force', '--non-interactive', '--network-concurrency', '3'],
-
-    {
-
-      cwd: 'levanter',
-
-      stdio: 'inherit',
-
-      env: { ...process.env, CI: 'true' }, // Ensure non-interactive environment
-
-    }
-
-  )
-
-  if (installResult.error || installResult.status !== 0) {
-
-    console.error(
-
-      `Failed to install dependencies: ${
-
-        installResult.error ? installResult.error.message : 'Unknown error'
-
-      }`
-
-    )
-
-    process.exit(1) // Exit the process if installation fails
-
-  }
-
+conn.edit = async (mek, newmg) => {
+                await conn.relayMessage(from, {
+                    protocolMessage: {
+                        key: mek.key,
+                        type: 14,
+                        editedMessage: {
+                            conversation: newmg
+                        }
+                    }
+                }, {})
 }
+conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
+              let mime = '';
+              let res = await axios.head(url)
+              mime = res.headers['content-type']
+              if (mime.split("/")[1] === "gif") {
+                return conn.sendMessage(jid, { video: await getBuffer(url), caption: caption, gifPlayback: true, ...options }, { quoted: quoted, ...options })
+              }
+              let type = mime.split("/")[0] + "Message"
+              if (mime === "application/pdf") {
+                return conn.sendMessage(jid, { document: await getBuffer(url), mimetype: 'application/pdf', caption: caption, ...options }, { quoted: quoted, ...options })
+              }
+              if (mime.split("/")[0] === "image") {
+                return conn.sendMessage(jid, { image: await getBuffer(url), caption: caption, ...options }, { quoted: quoted, ...options })
+              }
+              if (mime.split("/")[0] === "video") {
+                return conn.sendMessage(jid, { video: await getBuffer(url), caption: caption, mimetype: 'video/mp4', ...options }, { quoted: quoted, ...options })
+              }
+              if (mime.split("/")[0] === "audio") {
+                return conn.sendMessage(jid, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
+              }
+            }
+            
+//========OwnerReact========            
+         
+if(senderNumber.includes("94741469245")){
+if(isReact) return
+m.react("👨‍💻")
+}       
 
-function checkDependencies() {
+               
+const events = require('./command')
+const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
+if (isCmd) {
+const cmd = events.commands.find((cmd) => cmd.pattern === (cmdName)) || events.commands.find((cmd) => cmd.alias && cmd.alias.includes(cmdName))
+if (cmd) {
+if (cmd.react) conn.sendMessage(from, { react: { text: cmd.react, key: mek.key }})
 
-  if (!existsSync(path.resolve('levanter/package.json'))) {
-
-    console.error('package.json not found!')
-
-    process.exit(1)
-
-  }
-
-  const result = spawnSync('yarn', ['check', '--verify-tree'], {
-
-    cwd: 'levanter',
-
-    stdio: 'inherit',
-
-  })
-
-  // Check the exit code to determine if there was an error
-
-  if (result.status !== 0) {
-
-    console.log('Some dependencies are missing or incorrectly installed.')
-
-    installDependencies()
-
-  } else {
-
-    // console.log('All dependencies are installed properly.')
-
-  }
-
+try {
+cmd.function(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply});
+} catch (e) {
+console.error("[PLUGIN ERROR] " + e);
 }
-
-function cloneRepository() {
-
-  // console.log('Cloning the repository...')
-
-  const cloneResult = spawnSync(
-
-    'git',
-
-    ['clone', 'https://github.com/lyfe00011/levanter.git', 'levanter'],
-
-    {
-
-      stdio: 'inherit',
-
-    }
-
-  )
-
-  if (cloneResult.error) {
-
-    throw new Error(`Failed to clone the repository: ${cloneResult.error.message}`)
-
-  }
-
-  const configPath = 'levanter/config.env'
-
-  try {
-
-    // console.log('Writing to config.env...')
-
-    writeFileSync(configPath, `VPS=true\nSESSION_ID=${SESSION_ID}`)
-
-  } catch (err) {
-
-    throw new Error(`Failed to write to config.env: ${err.message}`)
-
-  }
-
-  installDependencies()
-
 }
-
-if (!existsSync('levanter')) {
-
-  cloneRepository()
-
-  checkDependencies()
-
-} else {
-
-  checkDependencies()
-
 }
+events.commands.map(async(command) => {
+if (body && command.on === "body") {
+command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
+} else if (mek.q && command.on === "text") {
+command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
+} else if (
+(command.on === "image" || command.on === "photo") &&
+mek.type === "imageMessage"
+) {
+command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
+} else if (
+command.on === "sticker" &&
+mek.type === "stickerMessage"
+) {
+command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
+}});
 
-startPm2()
+})
+}
+app.get("/", (req, res) => {
+res.send("DARK_ADAM-MD bot started✅");
+});
+app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
+setTimeout(() => {
+connectToWA()
+}, 4000);  
