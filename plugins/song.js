ecbb47f9885d28b8cce6fba75996d9d77ec2c420
Chanuka-madushan <chanukamadushan889@gmail.com>
2025-01-23 20:27:13 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -38,7 +38,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 1. Audio 🎧
 2. Document 🗂️
 
-> *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©DARK_ADAM-MD 💚*`;
+> *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©ＣＨＡＮＵＫＡ-ＭＤ*`;
 
         const vv = await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
 
